f160161ef155efa6aa9c6fa64c2a06d9b69de108
Steven Morgan <sum410@psu.edu>
2018-10-08 14:30:52 -0400

    Missing DV MAR

--- sim1.R
+++ sim1.R
@@ -8,7 +8,7 @@ library(evd)
 ### Uncorrelated data
 # Create df of uncorrelated data
 set.seed(24519)
-N <- 1000
+N <- 10000
 x1 <- rnorm(N, 0, 1)
 x2 <- runif(N, 0, 1)
 x3 <- rgumbel(N, 0, 1)
@@ -27,7 +27,7 @@ cor(x3, y)
 prob.m <- 0.05
 uncorr.mcar <- runif(N, min=0, max=1)
 y.mcar <- ifelse(uncorr.mcar < prob.m, NA, y)
-uncorr.missing <- (cbind(x1, x2, x3, y, y.mcar))
+uncorr.missing <- cbind(x1, x2, x3, y, y.mcar)
 
 # As expected, still not correlation between IV's and DV w/ MCAR
 cor(x1, y.mcar, use = 'complete.obs')
@@ -35,24 +35,20 @@ cor(x2, y.mcar, use = 'complete.obs')
 cor(x3, y.mcar, use = 'complete.obs')
 
 ## Missing at Random (MAR)
-y.mar = matrix(y, ncol=nj, nrow=ni, byrow=TRUE)
-for(i in 1:ni){
-  for(j in 4:nj){
-    dif1 = y.mar[i,j-2]-y.mar[i,j-3]
-    dif2 = y.mar[i,j-1]-y.mar[i,j-2]
-    if(dif1>0 & dif2>0){  # if weight goes up twice, drops out
-      y.mar[i,j:nj] = NA;  break
-    }
-  }
-}
-y.mar = as.vector(t(y.mar))
-View(cbind(id, week, cond, base, y, y.mar))
+#y.mar = cbind(x1, x2, x3, y)
+mod <- x1 - 0.08*x2 + x3 - 0.05 # Response model
+rp <- exp(mod) / (exp(mod) + 1) # Suppress values between 0 and 1 via inverse-logit
+y.mar <- rbinom(N, 1, rp)
+uncorr.mar <- as.data.frame(cbind(x1, x2, x3, y))
+uncorr.mar$y.mar <- ifelse(y.mar == 1, y, NA)
 
+cor(uncorr.mar$x1, uncorr.mar$y.mar, use = 'complete.obs')
+cor(uncorr.mar$x2, uncorr.mar$y.mar, use = 'complete.obs')
+cor(uncorr.mar$x3, uncorr.mar$y.mar, use = 'complete.obs')
 
 
 ## Create df of correlated dependent variable
 set.seed(1553)
-N <- 1000
 y <- rnorm(N)
 x1 <- y + rnorm(N, 0, 1)
 x2 <- y + runif(N, 0, 1)
@@ -66,8 +62,4 @@ cor(x2, x3)
 cor(x2, y)
 cor(x3, y)
 
-
-
-correlated <- cbind(c(x1, x2, x3, y))
-
-
+correlated <- cbind(x1, x2, x3, y)
