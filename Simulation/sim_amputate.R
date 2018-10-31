0ec02c5c5bbf4235c7b6f10ae1813f451718662c
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2018-10-31 12:53:03 -0400

    Upload ANES dataset with missingness based on MCAR, MAR, and MNAR

--- Simulation/sim_amputate.R
+++ Simulation/sim_amputate.R
@@ -1,26 +1,94 @@
+# This script adds missingness to the cleaned ANES data.
+
 rm(list=ls())
+setwd("C:/Users/Steve/Documents/GitHub/MIML/ANES")
 
 library(mice)
 library(MASS)
 
-#y <- rep(1, 10)
-#x <- matrix(1, nrow = 10, ncol = 5)
-#ry <- c(TRUE, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE)
-#data <- norm.draw(x, ry, y)
+seed = 24519
 
-set.seed(24519)
-test1 <- MASS::mvrnorm(n=1000, mu = rep(0, 10), Sigma = diag(10))
+### ANES Data -> Add missingness MCAR
 
-test1.miss <- ampute(test1, 0.2, mech = 'MCAR')
+load('cleanedANES.RData')
+
+# For "natural" missingngess, convert NA's to -999 (to distinguish from added missingness)
+anes[is.na(anes)] <- -999
+
+# Remove values -> MCAR
+set.seed(seed)
+anes.mcar <- ampute(anes, prop = 0.3, mech = 'MCAR')
+anes.mcar
+
+# Validate values were deleted MCAR -> Each row represents # of values removed 
+# based on the "pattern", columns represent # of cells removed from each 
+# variable, and maroon cells indicated removed values
+md.pattern(anes.mcar$amp)
+mypatterns <- anes.mcar$patterns
+mypatterns
+
+# Visualize missingness of both "natural" and removed data
+full.na.mcar <- anes.mcar$amp
+full.na.mcar[full.na.mcar == -999] <- NA
+md.pattern(full.na.mcar) # This looks wonky b/c there is no clear "pattern"
+
+# Compare proportion of missing data
+anes.mcar$prop
+
+# Determine number of incomplete observations across natural versus added df's
+sum(!complete.cases(anes.mcar$amp)) #1114
+sum(!complete.cases(full.na)) #2450
+
+amp.mcar <- anes.mcar$amp
+
+### ANES Data -> Add missingness MAR
+set.seed(seed)
+anes.mar <- ampute(anes, prop = 0.3, mech = 'MAR')
+anes.mar
+
+# Examine patterns of missingess
+md.pattern(anes.mar$amp)
+mypatterns <- anes.mar$patterns
+mypatterns
+
+# Visualize missingness of both "natural" and removed data
+full.na.mar <- anes.mar$amp
+full.na.mar[full.na.mar == -999] <- NA
+md.pattern(full.na.mar) # This looks wonky b/c there is no clear "pattern"
+
+# Compare proportion of missing data
+anes.mar$prop
+
+# Determine number of incomplete observations across natural versus added df's
+sum(!complete.cases(anes.mar$amp)) #1161
+sum(!complete.cases(full.na.mar)) #2612
+
+amp.mar <- anes.mar$amp
 
 
-# http://www.gerkovink.com/Amputation_with_Ampute/Vignette/ampute.html
-set.seed(2016)
-testdata <- MASS::mvrnorm(n = 10000, mu = c(10, 5, 0), Sigma = matrix(data = c(1.0, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 1.0), nrow = 3, byrow = T))
-testdata <- as.data.frame(testdata)
-summary(testdata)
+### ANES Data -> Add missingness MNAR
+set.seed(seed)
+anes.mnar <- ampute(anes, prop = 0.3, mech = 'MNAR')
+anes.mnar
 
-result <- ampute(testdata)
-result
+# Examine patterns of missingess
+md.pattern(anes.mnar$amp)
+mypatterns <- anes.mnar$patterns
+mypatterns
 
-md.pattern(result$amp)
+# Visualize missingness of both "natural" and removed data
+full.na.mnar <- anes.mnar$amp
+full.na.mnar[full.na.mnar == -999] <- NA
+md.pattern(full.na.mnar) # This looks wonky b/c there is no clear "pattern"
+
+# Compare proportion of missing data
+anes.mnar$prop
+
+# Determine number of incomplete observations across natural versus added df's
+sum(!complete.cases(anes.mnar$amp)) #1147
+sum(!complete.cases(full.na.mnar)) #2511
+
+amp.mnar <- anes.mnar$amp
+
+# Save six df's
+save(full.na.mcar, amp.mcar, full.na.mar, amp.mar, amp.mnar, full.na.mnar, file = 'anesMissing.RData')
