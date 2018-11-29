313cac89a3991dc83fe718d92ce8a7e9a8625c3e
Peng <eileen.pll@gmail.com>
2018-11-28 21:35:46 -0500

    upload ANES_missForest output

--- ANES/mult_imp_missForest.R
+++ ANES/mult_imp_missForest.R
@@ -52,9 +52,9 @@ out.mcar$ximp[cols2] <- lapply(out.mcar$ximp[cols2], as.numeric)
 #### Regression ####
 
 # Logit:
-## Model vote choice w/ feeling thermometers ##
-# logit_form <- as.formula(vote.dem ~ dem + gop + ideo + HRC.FT + DJT.FT + bible + inform + edu + income + female + black + hisp)
-# logit_raw <- glm(logit_form, data = ANES.mF$ximp, family=binomial(link='logit'))
+# Model vote choice w/ feeling thermometers ##
+logit_form <- as.formula(vote.dem ~ dem + gop + ideo + HRC.FT + DJT.FT + bible + inform + edu + income + female + black + hisp)
+logit.FT_mF <- glm(logit_form, data = ANES.mF$ximp, family=binomial(link='logit'))
 # logit_full <- glm(logit_form, data = anes.complete, family=binomial(link='logit'))
 # logit_mar <- glm(logit_form, data = out.mar$ximp, family=binomial(link='logit'))
 # logit_mnar <- glm(logit_form, data = out.mnar$ximp, family=binomial(link='logit'))
@@ -66,7 +66,7 @@ out.mcar$ximp[cols2] <- lapply(out.mcar$ximp[cols2], as.numeric)
 # summary(vote.dem.noFT)
 logit_form <- as.formula(vote.dem ~ dem + gop + ideo + bible + inform + edu + 
                              income + female + black + hisp)
-logit_raw <- glm(logit_form, data = ANES.mF$ximp, family=binomial(link='logit'))
+logit.noFT_mF <- glm(logit_form, data = ANES.mF$ximp, family=binomial(link='logit'))
 logit_full <- glm(logit_form, data = anes.complete, family=binomial(link='logit'))
 logit_mar <- glm(logit_form, data = out.mar$ximp, family=binomial(link='logit'))
 logit_mnar <- glm(logit_form, data = out.mnar$ximp, family=binomial(link='logit'))
@@ -75,45 +75,47 @@ logit_mcar <- glm(logit_form, data = out.mcar$ximp, family=binomial(link='logit'
 # OLS:
 # Model feeling thermometer for Trump (0-100 treated as continuous)
 ols_form <- as.formula(DJT.FT ~ dem + gop + ideo + bible + inform + edu + edu + income + female + black + hisp)
-ols_raw <- lm(ols_form, data = ANES.mF$ximp)
+ols_mF <- lm(ols_form, data = ANES.mF$ximp)
 ols_full <- lm(ols_form, data = anes.complete)
 ols_mar <- lm(ols_form, data = out.mar$ximp)
 ols_mnar <- lm(ols_form, data = out.mnar$ximp)
 ols_mcar <- lm(ols_form, data = out.mcar$ximp)
 
-## Logit: 
-stargazer(logit_raw, logit_full, logit_mar, logit_mnar, logit_mcar, title="Results", align=TRUE)
+save(logit.FT_mF, logit.noFT_mF, ols_mF, file = "ANES_missForest.RData")
 
-# Coefficients
-logit_bind_coeff <- cbind(as.numeric(logit_raw$coefficients),
-                          as.numeric(logit_full$coefficients),
-                          as.numeric(logit_mar$coefficients),
-                          as.numeric(logit_mnar$coefficients),
-                          as.numeric(logit_mcar$coefficients))
-colnames(logit_bind_coeff) <- c("raw", "full", "mar", "mnar", "mcar")
-
-# P-value? 
-# coef(summary(logit_raw))[,4]
-
-# Model fit
-logit_mod_f <- as.data.frame(t(c(logit_raw$aic,
-                                 logit_full$aic,
-                                 logit_mar$aic,
-                                 logit_mnar$aic,
-                                 logit_mcar$aic)))
-colnames(logit_mod_f) <- c("raw", "full", "mar", "mnar", "mcar")
-
-## OLS: 
-stargazer(ols_raw, ols_full, ols_mar, ols_mnar, ols_mcar, align = TRUE)
-
-# Coefficients
-
-ols_bind_coeff <- cbind(as.numeric(ols_raw$coefficients),
-                        as.numeric(ols_full$coefficients),
-                        as.numeric(ols_mar$coefficients),
-                        as.numeric(ols_mnar$coefficients),
-                        as.numeric(ols_mcar$coefficients))
-colnames(ols_bind_coeff) <- c("raw", "full", "mar", "mnar", "mcar")
-
-# P-value? 
-# coef(summary(ols_raw))[,4]
+# ## Logit: 
+# stargazer(logit_raw, logit_full, logit_mar, logit_mnar, logit_mcar, title="Results", align=TRUE)
+# 
+# # Coefficients
+# logit_bind_coeff <- cbind(as.numeric(logit_raw$coefficients),
+#                           as.numeric(logit_full$coefficients),
+#                           as.numeric(logit_mar$coefficients),
+#                           as.numeric(logit_mnar$coefficients),
+#                           as.numeric(logit_mcar$coefficients))
+# colnames(logit_bind_coeff) <- c("raw", "full", "mar", "mnar", "mcar")
+# 
+# # P-value? 
+# # coef(summary(logit_raw))[,4]
+# 
+# # Model fit
+# logit_mod_f <- as.data.frame(t(c(logit_raw$aic,
+#                                  logit_full$aic,
+#                                  logit_mar$aic,
+#                                  logit_mnar$aic,
+#                                  logit_mcar$aic)))
+# colnames(logit_mod_f) <- c("raw", "full", "mar", "mnar", "mcar")
+# 
+# ## OLS: 
+# stargazer(ols_raw, ols_full, ols_mar, ols_mnar, ols_mcar, align = TRUE)
+# 
+# # Coefficients
+# 
+# ols_bind_coeff <- cbind(as.numeric(ols_raw$coefficients),
+#                         as.numeric(ols_full$coefficients),
+#                         as.numeric(ols_mar$coefficients),
+#                         as.numeric(ols_mnar$coefficients),
+#                         as.numeric(ols_mcar$coefficients))
+# colnames(ols_bind_coeff) <- c("raw", "full", "mar", "mnar", "mcar")
+# 
+# # P-value? 
+# # coef(summary(ols_raw))[,4]
