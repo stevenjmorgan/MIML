6512a4b7d90dc908683983272b8eb1c0d1f962ad
ckelling <ckelling@vt.edu>
2018-11-30 09:20:16 -0500

    final implementation and output for stargazer

--- src_mult_imp/01_svm_mult_imp.R
+++ src_mult_imp/01_svm_mult_imp.R
@@ -5,7 +5,7 @@
 ###
 ### Implementation of the Protocol through SVM
 ### Created: 10/10/18
-### Last Updated: 11/9/18
+### Last Updated: 11/29/18
 ###
 
 # In this code, I will implement the protocol for the SODA 502 project for 
@@ -16,14 +16,7 @@ rm(list = ls())
 
 library(Amelia)
 library(caret)
-
-### Checking number of NA's per row
-sum_nacount <- apply(amp.mar, 1, function(x) sum(is.na(x)))
-length(which(sum_nacount>1))
-sum_nacount2 <- apply(amp.mnar, 1, function(x) sum(is.na(x)))
-length(which(sum_nacount2>1))
-sum_nacount3 <- apply(amp.mcar, 1, function(x) sum(is.na(x)))
-length(which(sum_nacount3>1))
+library(stargazer)
 
 ###
 ### Direct Imputation
@@ -39,6 +32,14 @@ load("C:/Users/ckell/Desktop/Google Drive/01_Penn State/2018-2019/Fall 2018/soda
 # Load SVM function
 source("C:/Users/ckell/Desktop/Google Drive/01_Penn State/2018-2019/Fall 2018/soda_502/project/MIML/src_mult_imp/00_svm_fnc.R")
 
+### Checking number of NA's per row
+sum_nacount <- apply(amp.mar, 1, function(x) sum(is.na(x)))
+length(which(sum_nacount>1))
+sum_nacount2 <- apply(amp.mnar, 1, function(x) sum(is.na(x)))
+length(which(sum_nacount2>1))
+sum_nacount3 <- apply(amp.mcar, 1, function(x) sum(is.na(x)))
+length(which(sum_nacount3>1))
+
 # Exploratory analysis:
 #par(mfrow=c(2,2))
 #missmap(anes, main = "Missing values vs observed, Full Data")
@@ -50,6 +51,20 @@ source("C:/Users/ckell/Desktop/Google Drive/01_Penn State/2018-2019/Fall 2018/so
 dim(anes)
 anes <- anes[complete.cases(anes),]
 dim(anes) #deletes 1889 rows
+anes <- as.data.frame(anes)
+
+# specify variable type
+cols1 <- c("vote.dem", "dem", "gop", "female", "black", "hisp", "white") # categorical
+cols2 <- c("ideo", "bible", "inform", "edu", "income") # ordinal
+# Use lapply() to coerce and replace the chosen columns to factors:
+anes[cols1] <- lapply(anes[cols1], factor)
+anes[cols2] <- lapply(anes[cols2], ordered)
+amp.mar[cols1] <- lapply(amp.mar[cols1], factor)
+amp.mar[cols2] <- lapply(amp.mar[cols2], ordered)
+amp.mnar[cols1] <- lapply(amp.mnar[cols1], factor)
+amp.mnar[cols2] <- lapply(amp.mnar[cols2], factor)
+amp.mcar[cols1] <- lapply(amp.mcar[cols1], factor)
+amp.mcar[cols2] <- lapply(amp.mcar[cols2], factor)
 
 
 # Then I impute values via train/test split or cross validation (preferably CV) 
@@ -69,17 +84,17 @@ fold1 <- amp.mar[flds[[1]],]
 #SVM through the function found in David Roger's thesis
 #SVMI<- function(data,categ.vars,modlist,max.iter=100,min.tol=1e-4)
 #    categ.vars is a vector indicating the column numbers of the categorical variables
-amp.mar <- amp.mar[,-7] #bible variable is causing some problems in the SVM
-amp.mnar <- amp.mnar[,-7] 
-amp.mcar <- amp.mcar[,-7] 
-colnames(amp.mar)
+# amp.mar <- amp.mar[,-7] #bible variable is causing some problems in the SVM
+# amp.mnar <- amp.mnar[,-7] 
+# amp.mcar <- amp.mcar[,-7] 
+# colnames(amp.mar)
 categ.vars <- c(1,2,3,4,7,8,9,10,11,12,13) #everything except "HRC.FT", "DJT.FT"
-
-# convert these columns to categorical
-amp.mar[categ.vars] <- lapply(amp.mar[categ.vars], factor)
-amp.mnar[categ.vars] <- lapply(amp.mnar[categ.vars], factor)
-amp.mcar[categ.vars] <- lapply(amp.mcar[categ.vars], factor)
-sapply(amp.mar, class)
+# 
+# # convert these columns to categorical
+# amp.mar[categ.vars] <- lapply(amp.mar[categ.vars], factor)
+# amp.mnar[categ.vars] <- lapply(amp.mnar[categ.vars], factor)
+# amp.mcar[categ.vars] <- lapply(amp.mcar[categ.vars], factor)
+# sapply(amp.mar, class)
 
 #    modlist should be a list containing the prefitted SVM models for each of the categ.vars
 trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
@@ -172,6 +187,15 @@ out.mar <- SVMI_all(amp.mar,categ.vars,modlist.all.mar,max.iter=100,min.tol=1e-4
 out.mnar <- SVMI_all(amp.mnar,categ.vars,modlist.all.mnar,max.iter=100,min.tol=1e-4)
 out.mcar <- SVMI_all(amp.mcar,categ.vars,modlist.all.mcar,max.iter=100,min.tol=1e-4)
 
+#check to make sure there are no missing values
+length(which(is.na(out.mar)))
+length(which(is.na(out.mcar)))
+length(which(is.na(out.mnar)))
+
+#number of missing values to start
+length(which(is.na(out.mar)))
+length(which(is.na(out.mcar)))
+length(which(is.na(out.mnar)))
 
 # I compare the imputed datasets against the original dataset (with no added missingness) 
 # to determine how well the algorithm recovers missing values. (This line might not be necessary)
@@ -186,80 +210,112 @@ out.mcar <- SVMI_all(amp.mcar,categ.vars,modlist.all.mcar,max.iter=100,min.tol=1
 # With the imputed dataset, I run two regressions:  logit and OLS.
 
 # Logit:
-logit_form <- as.formula(.~.)
-logit_full <- glm(logit_form, data = anes, family=binomial(link='logit'))
-logit_mar <- glm(logit_form, data = out.mar, family=binomial(link='logit'))
-logit_mnar <- glm(logit_form, data = out.mnar, family=binomial(link='logit'))
-logit_mcar <- glm(logit_form, data = out.mcar, family=binomial(link='logit'))
+# Model vote choice w/ feeling thermometers ##
+logit_form <- as.formula(vote.dem ~ dem + gop + ideo + HRC.FT + DJT.FT + bible + inform + edu + income + female + black + hisp)
+logit.FT_mF <- glm(logit_form, data = ANES.mF$ximp, family=binomial(link='logit'))
+# logit_full <- glm(logit_form, data = anes.complete, family=binomial(link='logit'))
+# logit_mar <- glm(logit_form, data = out.mar$ximp, family=binomial(link='logit'))
+# logit_mnar <- glm(logit_form, data = out.mnar$ximp, family=binomial(link='logit'))
+# logit_mcar <- glm(logit_form, data = out.mcar$ximp, family=binomial(link='logit'))
+
+## Model vote choice w/o feeling thermometers (reduce collinearity w/ ideology) ##
+# vote.dem.noFT <- glm(vote.dem ~ dem + gop + ideo + bible + inform + edu + 
+#                          income + female + black + hisp, data = ANES.mF$ximp, family = 'binomial')
+# summary(vote.dem.noFT)
+logit_form <- as.formula(vote.dem ~ dem + gop + ideo + bible + inform + edu + 
+                           income + female + black + hisp)
+logit.noFT_mF <- glm(logit_form, data = ANES.mF$ximp, family=binomial(link='logit'))
+logit_full <- glm(logit_form, data = anes.complete, family=binomial(link='logit'))
+logit_mar <- glm(logit_form, data = out.mar$ximp, family=binomial(link='logit'))
+logit_mnar <- glm(logit_form, data = out.mnar$ximp, family=binomial(link='logit'))
+logit_mcar <- glm(logit_form, data = out.mcar$ximp, family=binomial(link='logit'))
 
 # OLS:
-ols_form <- as.formula(.~.)
-ols_full <- lm(ols_form, data = anes)
-ols_mar <- lm(ols_form, data = out.mar)
-ols_mnar <- lm(ols_form, data = out.mnar)
-ols_mcar <- lm(ols_form, data = out.mcar)
+# Model feeling thermometer for Trump (0-100 treated as continuous)
+ols_form <- as.formula(DJT.FT ~ dem + gop + ideo + bible + inform + edu + edu + income + female + black + hisp)
+ols_mF <- lm(ols_form, data = ANES.mF$ximp)
+ols_full <- lm(ols_form, data = anes.complete)
+ols_mar <- lm(ols_form, data = out.mar$ximp)
+ols_mnar <- lm(ols_form, data = out.mnar$ximp)
+ols_mcar <- lm(ols_form, data = out.mcar$ximp)
 
-#First, I report the results: 
-###
-### Logit: 
-### 
-#     Coefficients
-logit_bind_coeff <- cbind(as.numeric(logit_full$coefficients),
-                    as.numeric(logit_mar$coefficients),
-                    as.numeric(logit_mnar$coefficients),
-                    as.numeric(logit_mcar$coefficients))
-colnames(logit_bind_coeff) <- c("full", "mar", "mnar", "mcar")
+save(logit.FT_mF, logit.noFT_mF, ols_mF, file = "C:/Users/ckell/Desktop/Google Drive/01_Penn State/2018-2019/Fall 2018/soda_502/project/MIML/ANES/ANES_svm.RData")
 
-#     P-value? Not sure if this is necessary? for coefficients or model?
-
-#     Model fit
-logit_mod_f <- as.data.frame(c(logit_full$aic,
-                               logit_mar$aic,
-                               logit_mnar$aic,
-                               logit_mcar$aic))
-colnames(logit_mod_f) <- c("full", "mar", "mnar", "mcar")
-###
-### OLS: 
-### 
-#     Coefficients
-ols_bind_coeff <- cbind(as.numeric(ols_full$coefficients),
-                        as.numeric(ols_mar$coefficients),
-                        as.numeric(ols_mnar$coefficients),
-                        as.numeric(ols_mcar$coefficients))
-colnames(ols_bind_coeff) <- c("full", "mar", "mnar", "mcar")
-
-#     P-value? Not sure if this is necessary? for coefficients or model?
-
-#     Model fit
-ols_mod_f <- as.data.frame(c(ols_full$aic,
-                               ols_mar$aic,
-                               ols_mnar$aic,
-                               ols_mcar$aic))
-colnames(ols_mod_f) <- c("full", "mar", "mnar", "mcar")
-
-
-###
-### Prediction
-###
-# Once again, I use the imputed data that is created above.
-# I compare the imputed simulated missing data to its original value.
-
-
-# I report the MAPE and MSE for the data. 
-act <- anes$vote.dem
-pred_mar <- out.mar$vote.dem
-pred_mnar <- out.mnar$vote.dem
-pred_mcar <- out.mcar$vote.dem
-
-#mse
-mse_mar <- mean((act-pred_mar)^2)
-mse_mnar <- mean((act-pred_mnar)^2)
-mse_mcar <- mean((act-pred_mcar)^2)
-
-#mape
-mape_mar <- mean(abs((act-pred_mar)/act))
-mape_mnar <- mean(abs((act-pred_mnar)/act))
-mape_mcar <- mean(abs((act-pred_mcar)/act))
+# # Logit:
+# logit_form <- as.formula(.~.)
+# logit_full <- glm(logit_form, data = anes, family=binomial(link='logit'))
+# logit_mar <- glm(logit_form, data = out.mar, family=binomial(link='logit'))
+# logit_mnar <- glm(logit_form, data = out.mnar, family=binomial(link='logit'))
+# logit_mcar <- glm(logit_form, data = out.mcar, family=binomial(link='logit'))
+# 
+# # OLS:
+# ols_form <- as.formula(.~.)
+# ols_full <- lm(ols_form, data = anes)
+# ols_mar <- lm(ols_form, data = out.mar)
+# ols_mnar <- lm(ols_form, data = out.mnar)
+# ols_mcar <- lm(ols_form, data = out.mcar)
+# 
+# #First, I report the results: 
+# ###
+# ### Logit: 
+# ### 
+# #     Coefficients
+# logit_bind_coeff <- cbind(as.numeric(logit_full$coefficients),
+#                     as.numeric(logit_mar$coefficients),
+#                     as.numeric(logit_mnar$coefficients),
+#                     as.numeric(logit_mcar$coefficients))
+# colnames(logit_bind_coeff) <- c("full", "mar", "mnar", "mcar")
+# 
+# #     P-value? Not sure if this is necessary? for coefficients or model?
+# 
+# #     Model fit
+# logit_mod_f <- as.data.frame(c(logit_full$aic,
+#                                logit_mar$aic,
+#                                logit_mnar$aic,
+#                                logit_mcar$aic))
+# colnames(logit_mod_f) <- c("full", "mar", "mnar", "mcar")
+# ###
+# ### OLS: 
+# ### 
+# #     Coefficients
+# ols_bind_coeff <- cbind(as.numeric(ols_full$coefficients),
+#                         as.numeric(ols_mar$coefficients),
+#                         as.numeric(ols_mnar$coefficients),
+#                         as.numeric(ols_mcar$coefficients))
+# colnames(ols_bind_coeff) <- c("full", "mar", "mnar", "mcar")
+# 
+# #     P-value? Not sure if this is necessary? for coefficients or model?
+# 
+# #     Model fit
+# ols_mod_f <- as.data.frame(c(ols_full$aic,
+#                                ols_mar$aic,
+#                                ols_mnar$aic,
+#                                ols_mcar$aic))
+# colnames(ols_mod_f) <- c("full", "mar", "mnar", "mcar")
+# 
+# 
+# ###
+# ### Prediction
+# ###
+# # Once again, I use the imputed data that is created above.
+# # I compare the imputed simulated missing data to its original value.
+# 
+# 
+# # I report the MAPE and MSE for the data. 
+# act <- anes$vote.dem
+# pred_mar <- out.mar$vote.dem
+# pred_mnar <- out.mnar$vote.dem
+# pred_mcar <- out.mcar$vote.dem
+# 
+# #mse
+# mse_mar <- mean((act-pred_mar)^2)
+# mse_mnar <- mean((act-pred_mnar)^2)
+# mse_mcar <- mean((act-pred_mcar)^2)
+# 
+# #mape
+# mape_mar <- mean(abs((act-pred_mar)/act))
+# mape_mnar <- mean(abs((act-pred_mnar)/act))
+# mape_mcar <- mean(abs((act-pred_mcar)/act))
 
 
 
